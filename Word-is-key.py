0d9f1afc7478c95ba4dada569fef19f8c5dabf58
Shibam Hazra <shibamhazra53@gmail.com>
2020-10-14 08:16:00 +0530

    Update Word-is-key.py

--- Word-is-key.py
+++ Word-is-key.py
@@ -1,1 +1,7 @@
-
+a = ["break", "case", "continue", "default", "defer", "else", "for", "func", "goto", "if", "map", "range", "return", "struct", "type", "var"]
+n = input()
+if n in a:
+  print(n,"is a keyword")
+else:
+  print(n,"is not a keyword")
+  
